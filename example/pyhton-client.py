c2cd07273b9335419b71ae0cdb03f79da49a05ea
Felix Schlitter <felixschlitter@gmail.com>
2015-02-07 21:37:57 +1300

    Remove example code, it's useless

--- example/pyhton-client.py
+++
@@ -1,8 +0,0 @@
-from socketIO_client import SocketIO
-
-
-print('con')
-with SocketIO('localhost', 1234, params={'type':'input'}) as socket:
-    print('con')
-    socket.emit('data', "**so was von egal**")
-
